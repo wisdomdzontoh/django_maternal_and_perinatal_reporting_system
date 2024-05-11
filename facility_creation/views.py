63862a70cb96b3498649b7f77586b24611d988e1
wisdomdzontoh <wisdomdzontoh@gmail>
2024-05-11 22:29:04 +0000

    added a message to form submissions

--- facility_creation/views.py
+++ facility_creation/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Region, District, Facility
 from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 
@@ -25,28 +26,64 @@ def index(request):
     }
     return render(request, 'facility_creation/index.html', context)
 
+
+
+
+
 @login_required(login_url="authentication:my-login")
 def add_region(request):
+    regions = Region.objects.all()
+    districts = District.objects.all()
     user = User.objects.all()
+    context = {
+        'regions': regions,
+      
+        'districts': districts,
+    }
     if request.method == 'POST':
-        region_name = request.POST.get("region_name")
-        region_code = request.POST.get("region_code")
-        date_created = request.POST.get("date_created")
-        created_by = request.POST.get("created_by")
+        if 'region_name' in request.POST and 'date_created' in request.POST:
+            region_name = request.POST.get("region_name")
+            region_code = request.POST.get("region_code")
+            date_created = request.POST.get("date_created")
+            created_by = request.POST.get("created_by")
+            
+            
+            if region_name and date_created:
+                region = Region(
+                    region_name=region_name,
+                    region_code=region_code,
+                    date_created=date_created,
+                    created_by=created_by,
+                )
+                region.save()
+                messages.success(request, 'Region added successfully.')
+                return redirect('add_region')  # Redirect to the same page after successful submission
         
-        if region_name != "" and date_created:
-            region = Region(
-                region_name=region_name, 
-                region_code=region_code,  
-                date_created=date_created,
-                created_by=created_by,
-            )
-            region.save()
-            return render(request, 'facility_creation/add_region.html', { 'success': True })
-        
-    else:
-        return render(request, 'facility_creation/add_region.html')
-    return render(request, 'facility_creation/add_region.html')
+        elif 'district_region' in request.POST and 'district_name' in request.POST:
+            district_region_id = request.POST.get("district_region")
+            district_name = request.POST.get("district_name")
+            district_code = request.POST.get("district_code")
+            date_created = request.POST.get("date_created")
+            created_by = request.POST.get("created_by")
+            
+            
+            if district_region_id and district_name:
+                district_region = Region.objects.get(id=district_region_id)
+                district = District(
+                    district_region=district_region,
+                    district_name=district_name,
+                    district_code=district_code,
+                    date_created=date_created,
+                    created_by=created_by,
+                )
+                district.save()
+                messages.success(request, 'District added successfully.')
+                return redirect('add_region')  # Redirect to the same page after successful submission
+
+   
+    return render(request, 'facility_creation/add_region.html', context)
+
+    
             
         
         
