cb369f7f7eecc3d36253b80136eaa29b82ee94e9
wisdomdzontoh <wisdomdzontoh@gmail>
2024-05-11 10:55:56 +0000

    first commit

---
+++ authentication/views.py
@@ -0,0 +1,60 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from . forms import CreateUserForm, LoginForm
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate, login, logout #authentication models views
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+def homepage(request):
+    
+    return render(request, 'authentication/index.html')
+
+def register(request):
+    
+    form = CreateUserForm()
+    if request.method == "POST":  # If the form has been submitted...
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("my-login")
+    context = {'registerform':  form}
+    
+    return render(request, 'authentication/register.html', context=context)
+
+def my_login(request):
+    
+    form = LoginForm()
+    
+    if request.method == 'POST':
+        
+        form = LoginForm(request, data=request.POST)
+        
+        if form.is_valid():
+            
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            
+            user = authenticate(request, username=username, password=password)
+            
+            if user is not None:
+                
+                auth.login(request, user)
+                
+                return redirect("dashboard")
+    
+    context = {'loginform': form }
+    
+    return render(request, 'authentication/my-login.html', context=context)
+
+def user_logout(request):
+    
+    auth.logout(request)
+    
+    return redirect("")
+
+
+
+@login_required(login_url="my-login")
+def dashboard(request):
+    return render(request, 'authentication/dashboard.html')
---
+++ maternaldeath/urls.py
@@ -0,0 +1,21 @@
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('authentication.urls')),  # authentication urls for login and signup pages
+    
+    
+    
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name="authentication/reset_password.html"), name="reset_password"),
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
+    
+     path("__reload__/", include("django_browser_reload.urls")),
+    
+]
