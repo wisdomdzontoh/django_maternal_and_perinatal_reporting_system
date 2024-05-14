7a93cfa440a3d83fad940f76b5dde9beaffefc0c
wisdomdzontoh <wisdomdzontoh@gmail>
2024-05-14 00:54:09 +0000

    added data entry form

--- maternal_entry/urls.py
+++ maternal_entry/urls.py
@@ -5,8 +5,8 @@ from . import views
 
 urlpatterns = [
     
-    
-    #path('maternal_entry', views.index, name="maternal_entry"),  # Adjusted URL pattern
+    path('maternal_entry/add_new', views.add_new, name="add_new"),  # Adjusted URL pattern
+    path('maternal_entry', views.index, name="maternal_entry"),  # Adjusted URL pattern
     
     # Authentication URLs ------------------------------------------------#
     path('authentication/user-logout/', auth_views.user_logout, name="user-logout"),  # Corrected import path and removed unnecessary comment
--- maternal_entry/views.py
+++ maternal_entry/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.http import JsonResponse
+from django.core.paginator import Paginator
+from facility_creation.models import Region, District, Facility
 
-# Create your views here.
+
+@login_required(login_url="authentication:my-login")
+def index(request):
+    return render(request, 'maternal_entry/index.html')
+
+
+@login_required(login_url="authentication:my-login")
+def add_new(request):
+    regions = Region.objects.all()
+    districts = District.objects.all()
+    context = {
+        'regions': regions,
+        'districts': districts,
+    }
+    return render(request, 'maternal_entry/add_new.html', context)
