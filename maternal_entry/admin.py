0383f4100dd4e05fc0c187f6c7bcb5d7fa6608fd
wisdomdzontoh <wisdomdzontoh@gmail>
2024-05-27 18:45:30 +0000

    updated files

--- maternal_entry/admin.py
+++ maternal_entry/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from maternal_entry.models import MaternalEntry
+from import_export.admin import ImportExportModelAdmin
+
+
 
 # Register your models here.
-admin.site.register(MaternalEntry)
+admin.site.register(MaternalEntry, ImportExportModelAdmin)
