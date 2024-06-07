b733e2e6eddf6c306b6f29c3f0318c8b43ed77d9
wisdomdzontoh <wisdomdzontoh@gmail>
2024-06-07 23:50:53 +0000

    updated login UI

--- maternaldeath/settings.py
+++ maternaldeath/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-*$)ftu(q9f-g5frxi%orv8x()(hke!y!_-xu1+#%r(^-!+uh0f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
